f1e559422616f8e96aaab721b688c191ac5bfaa3
Christopher A. Williamson <home@chrisaw.com>
2021-03-04 16:24:39 +0000

    Fix inputs

--- common/scripts/1_build_kernel.sh
+++ common/scripts/1_build_kernel.sh
@@ -14,7 +14,7 @@ ln -s /opt/toolchain /usr/src/kernel/toolchain
 
 # execute actual build
 ANDROID_MAJOR_VERSION="r"
-ANDROID_VERSION="11.0.0"
+PLATFORM_VERSION="11"
 export ANDROID_MAJOR_VERSION ANDROID_VERSION
 make -j$(nproc --all) ARCH=arm64 exynos9830-c2sxxx_defconfig
 make -j$(nproc --all) ARCH=arm64
