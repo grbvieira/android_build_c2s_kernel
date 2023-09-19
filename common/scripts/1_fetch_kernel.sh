b6fbd6550ae1a73dc47fe06b5e202a7807e2d8ec
Gerson Vieira <gersonbase@gmail.com>
2023-09-19 01:55:22 -0400

    update git in fetch_kernel

--- common/scripts/1_fetch_kernel.sh
+++ common/scripts/1_fetch_kernel.sh
@@ -3,5 +3,5 @@
 # fetch kernel sources
 git clone \
   -b ${1:-master} \
-  https://github.com/cawilliamson/android_kernel_samsung_c2s \
+  https://github.com/grbvieira/kernel-N986B.git \
   /home/noosreg/kernel
